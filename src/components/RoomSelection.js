3ced2178d0b3f32b04861eff92fd38e983c554f2
Isaac <isaacluiz0808@gmail.com>
2025-03-01 17:17:14 -0300

    Criando um RoomSelection.css; Importando RoomSelection.css no RoomSelection.js

--- src/components/RoomSelection.js
+++ src/components/RoomSelection.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import "./RoomSelection.css";
 const RoomSelection = ({ onRoomSelect }) => {
   const [roomName, setRoomName] = useState("");
   const [nickname, setNickname] = useState("");
