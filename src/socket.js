fb4f0d42a89853a330d8242fd79c905830f1934e
Isaac <isaacluiz0808@gmail.com>
2025-03-01 14:42:28 -0300

    Modificando o Socket.io para o jogador ser enviado a uma sala especifica

--- src/socket.js
+++ src/socket.js
@@ -2,4 +2,8 @@ import { io } from "socket.io-client";
 
 const socket = io("http://localhost:3000"); // Ajuste conforme necessário
 
+export const entrarSala = (sala, nomeJogador) => {
+    socket.emit("entrarSala", sala, nomeJogador);
+} 
+
 export default socket;
